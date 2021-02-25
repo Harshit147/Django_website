7379c1956b7dbd794e283a9b7b456e000b92c71a
Harshit147 <harshitparashar345@gmail.com>
2021-02-25 15:24:38 +0530

    commit

--- mac/shop/urls.py
+++ mac/shop/urls.py
@@ -1,14 +1,15 @@
-from django.urls import path ,include
+from django.urls import path, include
 from . import views
+
 urlpatterns = [
-    path("",views.index,name="ShopHome"),
-    path("Aboutus/",views.about_us,name="Aboutus"),
-    path("contact/",views.contact,name="contact"),
+    path("", views.index, name="ShopHome"),
+    path("Aboutus/", views.about_us, name="Aboutus"),
+    path("contact/", views.contact, name="contact"),
     path("products/<int:myid>", views.productView, name="ProductView"),
-    path("checkout/",views.checkout,name="checkout"),
-    path("tracker/",views.tracker,name="tracker"),
-    path("search/",views.search,name="search"),
-    path("register/",views.register,name="register"),
+    path("checkout/", views.checkout, name="checkout"),
+    path("tracker/", views.tracker, name="tracker"),
+    path("search/", views.search, name="search"),
+    path("register/", views.register, name="register"),
     path("handlerequest/", views.handlerequest, name="HandleRequest"),
     path("", include("django.contrib.auth.urls")),
 
